53830ba892f3171bb8bc053b79acfb344467b269
manny <manny@manny>
2017-05-11 18:52:16 +0300

    games moved

---
+++ sfml_by_example/views/Anim_Directional.h
@@ -0,0 +1,8 @@
+#include "Anim_Base.h"
+
+class Anim_Directional : public Anim_Base {
+    protected:
+    void FrameStep();
+    void CropSprite();
+    void ReadIn(std::stringstream& l_stream);
+};
